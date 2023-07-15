ba429b991309c218755c3ec6eabc6502a43acbb5
mrtuxa <mtuxa@leipzig.freifunk.net>
2023-07-15 12:06:44 +0200

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+macro_rules! pwint {
+    ($($arg:tt)*) => {
+        {
+            use std::io::Write;
+            write!(std::io::stdout(), $($arg)*).unwrap();
+        }
+    };
+}
+
+
+macro_rules! pwintwn {
+    () => {
+        println!("");
+    };
+    ($fmt:expr) => {
+        {
+            use std::io::Write;
+            writeln!(std::io::stdout(), $fmt).unwrap();
+        }
+    };
+    ($fmt:expr, $($arg:tt)*) => {
+        {
+            use std::io::Write;
+            writeln!(std::io::stdout(), $fmt, $($arg)*).unwrap();
+        }
+    };
+}
+
+macro_rules! epwint {
+    ($($arg:tt)*) => {
+        {
+            use std::io::Write;
+            write!(std::io::stderr(), $($arg)*).unwrap();
+        }
+    };
+}
+
+macro_rules! eprintwn {
+    () => {
+        eprintln!("");
+    };
+    ($fmt:expr) => {
+        {
+            use std::io::Write;
+            writeln!(std::io::stderr(), $fmt).unwrap();
+        }
+    };
+    ($fmt:expr, $($arg:tt)*) => {
+        {
+            use std::io::Write;
+            writeln!(std::io::stderr(), $fmt, $($arg)*).unwrap();
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        pwint!("One Line\n");
+        let hello = "Hello";
+        pwintwn!("{}", hello);
+        eprintwn!("Error");
+    }
+}
